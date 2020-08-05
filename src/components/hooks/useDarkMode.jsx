c444ebea2acbdbfbda0fc9db3834edfcc9064703
royeradames <royeraadames@gmail.com>
2020-08-05 16:26:21 -0400

    finish the useDarkMode.jsx file

---
+++ src/components/hooks/useDarkMode.jsx
@@ -0,0 +1,7 @@
+import useLocalStorage from './useLocalStorage'
+
+export const useDarkMode = (key) => {
+    // just simply composing useLocalStorage inside it and passing those values back to the component.
+    const [darkMode, setDarkMode] = useLocalStorage(key)
+    return [darkMode, setDarkMode]
+} 
